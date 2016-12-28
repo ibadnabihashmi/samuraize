e623fbe4fd5e004a0c4a6a4ebaa036147f91db42
ehashmi <ehashmi@etilizepak.com>
2016-12-28 12:36:33 +0500

    modified

--- extension/popup.js
+++ extension/popup.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
+  $('#loader-cont').hide();
   $('#samuraize').click(function () {
+    $('#loader-cont').show();
+    $('#samuraize').hide();
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {action:'GET_LOCATION'}, function(response) {
         $.ajax({
           method: 'post',
-          url: 'http://107.170.243.227:3004/samuraize',
+          url: 'http://localhost:3004/samuraize',
           dataType: 'json',
           data: {
             uri:response.location
@@ -15,7 +18,8 @@ $(document).ready(function () {
               summary: res.summary,
               title: res.title
             },function (response) {
-
+              $('#loader-cont').hide();
+              $('#samuraize').show();
             });
           }
         });
--- extension/samurai.js
+++ extension/samurai.js
@@ -7,7 +7,83 @@ setTimeout(function(){
             sendResponse({location: document.location.href});
             break;
           case 'INIT_MODAL':
-            var css = '.modal { display: none; /* Hidden by default */ position: fixed; /* Stay in place */ z-index: 1; /* Sit on top */ left: 0; top: 0; width: 100%; /* Full width */ height: 100%; /* Full height */ overflow: auto; /* Enable scroll if needed */ background-color: rgb(0,0,0); /* Fallback color */ background-color: rgba(0,0,0,0.4); /* Black w/ opacity */ -webkit-animation-name: fadeIn; /* Fade in the background */ -webkit-animation-duration: 0.4s; animation-name: fadeIn; animation-duration: 0.4s } /* Modal Content */ .modal-content { position: fixed; bottom: 0; background-color: #fefefe; width: 100%; -webkit-animation-name: slideIn; -webkit-animation-duration: 0.4s; animation-name: slideIn; animation-duration: 0.4s } /* The Close Button */ .close { color: white; float: right; font-size: 28px; font-weight: bold; } .close:hover, .close:focus { color: #000; text-decoration: none; cursor: pointer; } .modal-header { padding: 2px 16px; background-color: #5cb85c; color: white; } .modal-body {padding: 2px 16px;} .modal-footer { padding: 2px 16px; background-color: #5cb85c; color: white; } /* Add Animation */ @-webkit-keyframes slideIn { from {bottom: -300px; opacity: 0} to {bottom: 0; opacity: 1} } @keyframes slideIn { from {bottom: -300px; opacity: 0} to {bottom: 0; opacity: 1} } @-webkit-keyframes fadeIn { from {opacity: 0} to {opacity: 1} } @keyframes fadeIn { from {opacity: 0} to {opacity: 1} }';
+            var css = '' +
+              '.modal-samurai ' +
+              '{ ' +
+                'display: none; /* Hidden by default */ ' +
+                'position: fixed; /* Stay in place */ ' +
+                'z-index: 9999999; /* Sit on top */ ' +
+                'left: 0; ' +
+                'top: 0; ' +
+                'width: 100%; /* Full width */ ' +
+                'height: 100%; /* Full height */ ' +
+                'overflow: auto; /* Enable scroll if needed */ ' +
+                'background-color: rgb(0,0,0); /* Fallback color */ ' +
+                'background-color: rgba(0,0,0,0.4); /* Black w/ opacity */ ' +
+                '-webkit-animation-name: fadeIn; /* Fade in the background */ ' +
+                '-webkit-animation-duration: 0.4s; ' +
+                'animation-name: fadeIn; ' +
+                'animation-duration: 0.4s;' +
+                'font-family: \'Space Mono\', monospace; ' +
+              '} ' +
+              '/* Modal Content */ ' +
+              '.modal-content-samurai ' +
+              '{ ' +
+                'position: fixed; ' +
+                'bottom: 0; ' +
+                'background-color: #fefefe; ' +
+                'width: 100%; ' +
+                '-webkit-animation-name: slideIn; ' +
+                '-webkit-animation-duration: 0.4s; ' +
+                'animation-name: slideIn; ' +
+                'animation-duration: 0.4s ' +
+                'font-family: \'Space Mono\', monospace; ' +
+              '} ' +
+              '/* The Close Button */ ' +
+              '.close-samurai ' +
+              '{ ' +
+                'color: white; ' +
+                'float: right; ' +
+                'font-size: 28px; ' +
+                'font-weight: bold; ' +
+              '} ' +
+              '.close-samurai:hover, .close-samurai:focus ' +
+              '{ ' +
+                'color: #000; ' +
+                'text-decoration: none; ' +
+                'cursor: pointer; ' +
+              '} ' +
+              '.modal-header-samurai ' +
+                '{ ' +
+                'padding: 2px 16px; ' +
+                'background-color: #5cb85c; ' +
+                'color: white; ' +
+                'font-family: \'Space Mono\', monospace; ' +
+              '} ' +
+              '.modal-body-samurai ' +
+              '{' +
+                'padding: 2px 16px;' +
+                'font-family: \'Space Mono\', monospace; ' +
+              '} ' +
+              '.modal-footer-samurai ' +
+              '{ ' +
+                'padding: 2px 16px; ' +
+                'background-color: #5cb85c; ' +
+                'color: white; ' +
+              '} ' +
+              '/* Add Animation */ ' +
+              '@-webkit-keyframes slideIn ' +
+              '{ ' +
+                'from {bottom: -300px; opacity: 0} to {bottom: 0; opacity: 1} } ' +
+              '@keyframes slideIn ' +
+              '{ ' +
+                'from {bottom: -300px; opacity: 0} to {bottom: 0; opacity: 1} } ' +
+              '@-webkit-keyframes fadeIn ' +
+              '{ ' +
+                'from {opacity: 0} to {opacity: 1} } ' +
+              '@keyframes fadeIn ' +
+              '{ ' +
+                'from {opacity: 0} to {opacity: 1} }';
             style = document.createElement('style');
 
             if (style.styleSheet) {
@@ -21,13 +97,13 @@ setTimeout(function(){
               _summary += '<p>'+sum+'</p>';
             });
             var e = document.createElement('div');
-            e.innerHTML = '<div id="myModal" class="modal"> ' +
-                '<div class="modal-content"> ' +
-                '<div class="modal-header">' +
-                '<span class="close">&times;</span> ' +
+            e.innerHTML = '<div id="myModalSamurai" class="modal-samurai"> ' +
+                '<div class="modal-content-samurai"> ' +
+                '<div class="modal-header-samurai">' +
+                '<span class="close-samurai">&times;</span> ' +
                 '<h2>'+request.title+'</h2> ' +
                 '</div> ' +
-                '<div class="modal-body"> ' +
+                '<div class="modal-body-samurai"> ' +
                 '<p>'+_summary +'</p>'+
                 '</div> ' +
                 '</div> ' +
@@ -35,7 +111,7 @@ setTimeout(function(){
 
             document.getElementsByTagName('head')[0].appendChild(style);
             document.getElementsByTagName('body')[0].appendChild(e);
-            var myModal = document.getElementById('myModal');
+            var myModal = document.getElementById('myModalSamurai');
             var span = document.getElementsByClassName("close")[0];
             span.onclick = function() {
               myModal.style.display = "none";
@@ -46,6 +122,7 @@ setTimeout(function(){
               }
             };
             myModal.style.display = "block";
+            sendResponse({status: 'done'});
             break;
         }
       });
